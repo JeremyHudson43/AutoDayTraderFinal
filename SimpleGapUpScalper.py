464684a8cccfe9243be139a0c0c7072a16ce752a
JeremyHudson43 <jer4349@gmail.com>
2022-01-18 11:09:36 -0500

    Update SimpleGapUpScalper.py

--- SimpleGapUpScalper.py
+++ SimpleGapUpScalper.py
@@ -1,90 +1,85 @@
-import SimpleGapUpScalper
-import GetGapUps
-import pandas as pd
 import time
-from datetime import datetime
-import traceback
+from ib_insync.contract import Stock
+from ib_insync import Order
 import sys
-from ib_insync.ib import IB
-import random
 
-scalper = SimpleGapUpScalper.GapUpScalper_Driver()
-get_gappers_class = GetGapUps.GetGapper_Driver()
+class GapUpScalper_Driver():
 
-# Logging into Interactive Broker TWS
-ib = IB()
-ib.connect('127.0.0.1', 7497, clientId=random.randint(0, 300))
+    def get_percent(self, first, second):
+        if first == 0 or second == 0:
+            return 0
+        else:
+            percent = first / second * 100
+        return percent
 
-def check_time():
-    ## STARTING THE ALGORITHM ##
-    # Time frame: 6.30 hrs
+    def sell_stock(self, ib):
 
-    now = str(datetime.now().time())  # time object
+       ib.reqGlobalCancel()
+       
+       if len(ib.openOrders()) > 0 and len([v.position for v in ib.positions()][0]) > 0:
 
-    StartTime = pd.to_datetime("9:30").tz_localize('America/New_York')
-    TimeNow = pd.to_datetime(now).tz_localize('America/New_York')
-    EndTime = pd.to_datetime("16:00").tz_localize('America/New_York')
+           ticker = ib.positions()[0]
+           qty = [v.position for v in ib.positions()][0]
+    
+           if qty > 0:
+               ticker_contract = Stock(ticker, 'SMART', 'USD')
+    
+               order = Order(orderId=30, action='Sell', orderType='MKT', totalQuantity=qty)
+    
+               ib.placeOrder(ticker_contract, order)
+    
+               print('Sold ' + str(ticker) +  "!")
+    
+               time.sleep(10)
+    
+               sys.exit(0)
 
-    time_until_market_close = (EndTime - TimeNow).total_seconds()
+    def buy_stock(self, ticker, premarket_high, multiplier, ib, purchased):
 
-    # Waiting for Market to Open
-    if StartTime > TimeNow:
-        wait = (StartTime - TimeNow).total_seconds()
-        print("Waiting for Market to Open..")
-        print(f"Sleeping for {wait} seconds")
-        time.sleep(wait)
+       ticker_contract = Stock(ticker, 'SMART', 'USD')
 
-    return StartTime, TimeNow, EndTime, time_until_market_close
+       [ticker_close] = ib.reqTickers(ticker_contract)
 
+       within_limit_range = premarket_high * 1.005 < ticker_close.marketPrice() * 1.095
 
-def generate_gapper_CSV():
+       if within_limit_range:
 
-    today = datetime.today().strftime('%Y-%m-%d')
+           acc_vals = float([v.value for v in ib.accountValues() if v.tag == 'CashBalance' and v.currency == 'USD'][0])
 
-    filepath = 'C:\\Users\\Frank Einstein\\PycharmProjects\\AutoDaytrader\\gapper_records\\gappers_' + today + '.csv'
+           limit_price = float(str(round(premarket_high * 1.005, 2)))
 
-    df = get_gappers_class.get_gappers()
-    df.to_csv(filepath)
+           percent_of_acct_to_trade = 0.05
 
-    return df
+           qty = (acc_vals // limit_price) * percent_of_acct_to_trade
+           qty = round(qty)
 
+           pct_difference = round(self.get_percent((qty * limit_price), acc_vals), 2)
 
-if __name__ == "__main__":
+           limit_market_difference = 100 - self.get_percent(ticker_close.marketPrice(), limit_price)
 
-    orders = []
+           print("Limit Price:", limit_price)
+           print("Current Price:", ticker_close.marketPrice())
 
-    check_time()
+           print("Difference", str(round(abs(limit_market_difference), 2)) + "%")
+           print("")
 
-    df = generate_gapper_CSV()
+           # if abs(limit_market_difference) < 0.5 and ticker_close.marketPrice() >= limit_price:
 
-    tickers = df['Ticker'].to_list()
-    premarket_highs = df['Premarket High'].to_list()
+           print('\nYou bought ' + str(qty) + ' shares of ' + str(ticker) +
+                 ' for a total of $' + str(round(qty * limit_price)) + ' USD' +
+                 ' which is ' + str(pct_difference) + '% of your account ')
 
-    multiplier = 0
-    purchased = False
+           buy_order = Order(orderId=5 * multiplier, action='BUY', orderType='MKT', totalQuantity=qty)
 
-    start_time, time_now, end_time, time_until_market_close = check_time()
+           ib.placeOrder(ticker_contract, buy_order)
 
-    while time_now < end_time:
-        start_time, time_now, end_time, time_until_market_close = check_time()
+           time.sleep(15)
 
-        for ticker, premarket_high in zip(tickers, premarket_highs):
+           sell_order = Order(orderId=10 * multiplier, action='Sell', orderType='TRAIL',
+                         trailingPercent=2.0, totalQuantity=qty)
 
-            print("Ticker", ticker)
-            print("Premarket High", premarket_high)
-            print("")
+           # ib.placeOrder(ticker_contract, sell_order)
 
-            multiplier = multiplier + 1
+           purchased = True
 
-            if not purchased:
-                purchased = scalper.buy_stock(ticker, premarket_high, multiplier, ib, purchased)
-            elif purchased:
-
-                print('Purchased! Sleeping until 15 minutes before market close')
-                
-                time.sleep(time_until_market_close - 900)
-                scalper.sell_stock(ib)
-
-        time.sleep(5)
-
-    sys.exit(0)
+       return purchased
